cddd822f72e1cf80dbdfcba9f137ac0b1467c137
cosmicb0y <jang.hw73@gmail.com>
2018-05-17 16:48:12 +0900

    first commit

---
+++ src/main/java/com/hackday/anigif/AnigifApplication.java
@@ -0,0 +1,12 @@
+package com.hackday.anigif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnigifApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnigifApplication.class, args);
+    }
+}
